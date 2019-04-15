dc5480ed3a413f16681f2246761f1881a2fbf843
extremania <beforu@qq.com>
2019-04-15 22:45:16 +0800

    initial

---
+++ leetcode-wtf/src/com/github/extermania/leetcode/wtf/$0010_Regular_Expression_Matching.java
@@ -0,0 +1,9 @@
+package com.github.extermania.leetcode.wtf;
+
+public class $0010_Regular_Expression_Matching {
+	class Solution {
+	    public boolean isMatch(String s, String p) {
+	       return s.matches(p); //what the... accepted?
+	    }
+	}
+}
