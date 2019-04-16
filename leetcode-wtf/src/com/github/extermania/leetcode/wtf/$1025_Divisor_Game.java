b1f3bdf54512c6901b0fe0ac4dd70d4e6df99376
extremania <beforu@qq.com>
2019-04-16 22:26:42 +0800

    solutions of 1025

---
+++ leetcode-wtf/src/com/github/extermania/leetcode/wtf/$1025_Divisor_Game.java
@@ -0,0 +1,9 @@
+package com.github.extermania.leetcode.wtf;
+
+public class $1025_Divisor_Game {
+	class Solution {
+	    public boolean divisorGame(int N) {
+	        return N%2==0; //em...
+	    }
+	}
+}
