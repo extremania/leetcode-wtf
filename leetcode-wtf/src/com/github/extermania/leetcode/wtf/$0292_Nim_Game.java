72aa1d7ff6db1dd628f070e907b8e8ccdc395b22
extremania <beforu@qq.com>
2019-04-25 22:34:49 +0800

    solution of 292

---
+++ leetcode-wtf/src/com/github/extermania/leetcode/wtf/$0292_Nim_Game.java
@@ -0,0 +1,9 @@
+package com.github.extermania.leetcode.wtf;
+
+public class $0292_Nim_Game {
+	class Solution {
+	    public boolean canWinNim(int n) {
+	        return n%4==0?false:true;
+	    }
+	}
+}
